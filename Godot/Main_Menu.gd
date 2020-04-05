1d31d3353beff99919bc30846831a15d44523ae1
Mareta Dagostino <MaretaDagostino@users.noreply.github.com>
2020-04-06 00:10:33 +0200

    FPS tutorial part 6
    The changes are explained in part 6 of the Godot FPS tutorial:
    https://docs.godotengine.org/en/3.2/tutorials/3d/fps_tutorial/part_six.html
    
    As final code base commit 731e258 (27 Mar 2019) from GitHub was used:
    https://github.com/TwistedTwigleg/Godot_FPS_Tutorial
    
    Removed warnings (except 1 remaining deprecated method) and adjusted sounds.

---
+++ Godot/Main_Menu.gd
@@ -0,0 +1,83 @@
+extends Control
+
+# NOTE: The code here is only briefly explained in the tutorial, nor
+#		does the code have very many comments!
+#		This is because it does not really relate to making a FPS.
+
+var start_menu
+var level_select_menu
+var options_menu
+
+export (String, FILE) var testing_area_scene
+
+
+func _ready():
+	start_menu = $Start_Menu
+	level_select_menu = $Level_Select_Menu
+	options_menu = $Options_Menu
+	
+	# Connect all of the start menu buttons
+	var _err = $Start_Menu/Button_Start.connect("pressed", self, "start_menu_button_pressed", ["start"])
+	_err = $Start_Menu/Button_Open_Godot.connect("pressed", self, "start_menu_button_pressed", ["open_godot"])
+	_err = $Start_Menu/Button_Options.connect("pressed", self, "start_menu_button_pressed", ["options"])
+	_err = $Start_Menu/Button_Quit.connect("pressed", self, "start_menu_button_pressed", ["quit"])
+	
+	# Connect all of the level select menu buttons
+	_err = $Level_Select_Menu/Button_Back.connect("pressed", self, "level_select_menu_button_pressed", ["back"])
+	_err = $Level_Select_Menu/Button_Level_Testing_Area.connect("pressed", self, "level_select_menu_button_pressed", ["testing_scene"])
+	
+	# Connect all of the options menu buttons
+	_err = $Options_Menu/Button_Back.connect("pressed", self, "options_menu_button_pressed", ["back"])
+	_err = $Options_Menu/Button_Fullscreen.connect("pressed", self, "options_menu_button_pressed", ["fullscreen"])
+	_err = $Options_Menu/Check_Button_VSync.connect("pressed", self, "options_menu_button_pressed", ["vsync"])
+	_err = $Options_Menu/Check_Button_Debug.connect("pressed", self, "options_menu_button_pressed", ["debug"])
+	
+	# Some times when returning to the title screen the mouse is still captured even though it shouldn't be.
+	# To prevent this from breaking the game, we just set it here too
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	# Get the globals singleton
+	var globals = get_node("/root/Globals")
+	# Get the mouse sensitivity
+	$Options_Menu/HSlider_Mouse_Sensitivity.value = globals.mouse_sensitivity
+
+
+func start_menu_button_pressed(button_name):
+	if button_name == "start":
+		level_select_menu.visible = true
+		start_menu.visible = false
+	elif button_name == "open_godot":
+		var _err = OS.shell_open("https://godotengine.org/")
+	elif button_name == "options":
+		options_menu.visible = true
+		start_menu.visible = false
+	elif button_name == "quit":
+		get_tree().quit()
+
+
+func level_select_menu_button_pressed(button_name):
+	if button_name == "back":
+		start_menu.visible = true
+		level_select_menu.visible = false
+	elif button_name == "testing_scene":
+		set_mouse_sensitivity()
+		get_node("/root/Globals").load_new_scene(testing_area_scene)
+
+
+func options_menu_button_pressed(button_name):
+	if button_name == "back":
+		start_menu.visible = true
+		options_menu.visible = false
+	elif button_name == "fullscreen":
+		OS.window_fullscreen = !OS.window_fullscreen
+	elif button_name == "vsync":
+		OS.vsync_enabled = $Options_Menu/Check_Button_VSync.pressed
+	elif button_name == "debug":
+		get_node("/root/Globals").set_debug_display($Options_Menu/Check_Button_Debug.pressed)
+
+
+func set_mouse_sensitivity():
+	# Get the globals singleton
+	var globals = get_node("/root/Globals")
+	# Set the mouse sensitivity
+	globals.mouse_sensitivity = $Options_Menu/HSlider_Mouse_Sensitivity.value
